698b95c5fa3063daf60183f9b8994bf5ce999cf7
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-10-15 01:45:04 -0300

    agrega la funcion findKnapsac

--- algoritmosGeneticos/boundknapsack/geneticEngine.py
+++ algoritmosGeneticos/boundknapsack/geneticEngine.py
@@ -1,39 +1,95 @@
 from algoritmosGeneticos.boundknapsack.knapsuite import *
 
-
-def initPopulation(populationLen,W):
+#inicia una poblacion de largo population len y el largo de la mochila es el largo de W
+def initPopulation(populationLen,V,W,maxW):
     population=[]
-
     for i in range(populationLen):
-
-        population.append(generateRandomknapsack(W))
-
-
+        population.append(knapsack(V,W,maxW))
     return population
 
-
-
-
-def fitnesses(population,V,W,maxW):
+#calcula el fitness de cada mochila segun su V, W y maxW
+def fitnesses(population):
     fitness=[]
-
     for p in population:
-        fitness.append(knapsackFit(V,W,maxW))
-
+        fitness.append(p.fitness())
     return fitness
 
-
+#devuelve un arreglo de largo n, el que contiene numeros aleatoreos entre 0 y l-1
+def randIndex(l,n):
+    r=[]
+    for i in range(n):
+        r.append(random.randint(0, l - 1))
+    return r
 
 ##############################################################
 
 
-def findKnapSack(W,V,trys):
+def findKnapSack(V,W,maxW,trys,populationLen,Ncompetitors):
     print("buscando knapsack para W="+str(W)+" y V="+str(V))
 
+    population = initPopulation(populationLen,V,W,maxW)
+
+    #como queremos encontrar la mejor mochila, hacemos trys iteraciones
+    for t in range(trys):
+
+        #aca se ira guardando la desendencia de la nueva poblacion.
+
+        newPopulation=[]
+
+        #queremos que el largo de la poblacion sea  el mismo siempre, por lo que vamos
+        #agregando nuevos valores a newPopulation hasta que sus valores sean iguales.
+
+        while(len(newPopulation)<len(population)):
+
+            #buscamos indices al azar
+            competitorsI=randIndex(len(population),Ncompetitors)
+
+            #buscamos a los competidores, por los indices anteriores
+            competitors=[]
+            for i in competitorsI:
+                competitors.append(population[i])
+
+
+            #calculamos sus fitness, tal que fitness corresponde 1 a 1 con competitors y competitorsI
+            fitness=fitnesses(competitors)
+            # tournament
+            max1 = -1
+            max2 = -1
+            winner1 = competitors[0]
+            winner2 = competitors[0]
+            for i in range(len(competitors)):
+                if fitness[i] > max1:
+                    max1=fitness[i]
+                    winner1 = competitors[i]
+
+            for i in range(len(competitors)):
+                if competitors[i] != winner1 and fitness[i] > max2:
+                    max2=fitness[i]
+                    winner2 = competitors[i]
+
+            son = knapCrossOver(winner1,winner2)
+            newPopulation.append(son)
+
+        population=newPopulation
+
+    maxfit=-1
+    best=0
+    F=fitnesses(population)
+    for i in range(len(F)):
+        if F[i]>maxfit:
+            maxfit=F[i]
+            best=i
 
 
 
-findKnapSack([10,2,3],[1,3,4],100)
+    population[best].summary()
+
+W=[12,2,1,1,4]
+V=[4,2,2,1,10]
+maxW=15
+
+
+findKnapSack(V,W,maxW,10,10,3)
 
 
 
--- algoritmosGeneticos/boundknapsack/knapsuite.py
+++ algoritmosGeneticos/boundknapsack/knapsuite.py
@@ -1,24 +1,73 @@
 import random
 
-#genera un encode para un knapsack, donde un 1 en la posicion i, representa que dicho elemento esta en la mochila
-def generateRandomknapsack(W):
-    knapsack=[]
-    for i in range(len(W)):
-        knapsack.append(random.randint(0,1))
-    return knapsack
 
-#calcula el fit de una mochila (segun sus valores, sus pesos y su peso maximo)
-def knapsackFit(V,W,maxW):
-    value=sum(V)
-    weights=sum(W)
-    if weights<=maxW:
-        return value
+class knapsack:
+    def __init__(self, V, W,maxW):
+        self.V = V
+        self.W = W
+        self.maxW=maxW
+        self.K = []
+        for i in range(len(W)):
+            self.K.append(random.randint(0, 1))
+
+    def getV(self):
+        return self.V
+
+    def getW(self):
+        return self.W
+
+    def getK(self):
+        return self.K
+
+    def setK(self,K):
+        self.K=K
+
+    def getMaxW(self):
+        return self.maxW
+
+    def fitness(self):
+        value = sum(self.V)
+        weights = sum(self.W)
+        if weights <= self.maxW:
+            return value
+        else:
+            # si la mochila exede el peso, se castiga.
+            return value - self.autoPunishment(weights, self.maxW)
+
+    # funcion de castigo (puede cambiar para experimentar)
+    # si una mochila se pasa del peso, se le resta la diferencia a su valor.
+    def autoPunishment(self,w, maxW):
+        return w - maxW
+
+    def summary(self):
+        print("V="+str(self.V))
+        print("W="+str(self.W))
+        print("K=" + str(self.K))
+        print("fitness " + str(self.fitness()))
+
+def knapCrossOver(k1,k2):
+
+    A1=k1.getK()
+    A2=k2.getK()
+
+    l = len(A1)
+    gen = random.randint(0, l)
+    k = []
+
+    raux = random.randint(0, 1)
+    if raux == 0:
+        for i in range(l):
+            if i <= gen:
+                k.append(A1[i])
+            else:
+                k.append(A2[i])
     else:
-        #si la mochila exede el peso, se castiga.
-        return value-punishment(weights,maxW)
+        for i in range(l):
+            if i <= gen:
+                k.append(A2[i])
+            else:
+                k.append(A1[i])
 
-
-#funcion de castigo (puede cambiar para experimentar)
-#si una mochila se pasa del peso, se le resta la diferencia a su valor.
-def punishment(w,maxW):
-    return w-maxW
+    kson=knapsack(k1.getV(),k1.getW(),k1.getMaxW())
+    kson.setK(k)
+    return kson
